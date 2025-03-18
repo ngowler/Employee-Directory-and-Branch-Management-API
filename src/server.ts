7d60699a6749e3bc17eb3a5fbb17a162cac7430c
Nick Gowler <ngowler@academic.rrc.ca>
2025-03-18 10:15:22 -0500

    Updated server to make better use of environment variables.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import{ Server } from "http";
 
-const PORT: string | 3000 = process.env.PORT || 3000;
+const PORT: string | number = process.env.PORT || 3000;
 
 const server: Server = app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
